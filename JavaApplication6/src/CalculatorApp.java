1d96fd664f73091b13b6ee5e1d5b3e45765ae159
CrySoulllll <mr.yaroskaw@gmail.com>
2025-02-19 12:02:13 +0300

    Nachalo2

--- JavaApplication6/src/CalculatorApp.java
+++ JavaApplication6/src/CalculatorApp.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
 /**
@@ -8,5 +8,12 @@
  * @author Ярослав
  */
 public class CalculatorApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
