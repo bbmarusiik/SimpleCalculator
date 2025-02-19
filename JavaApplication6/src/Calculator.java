ab5dc58e08788117d5ce1137896f1cc7a7a087dc
bbmarusiik <ms.marusia05@mail.ru>
2025-02-19 13:31:14 +0300

    masha22

--- JavaApplication6/src/Calculator.java
+++ JavaApplication6/src/Calculator.java
@@ -1,12 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Ярослав
- */
 public class Calculator {
     
+    public double multiply (double a, double b) {
+        return a*b;
+    }
+    
+    public double divide (double a, double b) {
+        return a/b;
+    }
 }
