3ee18b420bffaf4dd2824450a219237939952f4a
kwqrina <ekaterina.motruk@gmail.com>
2025-02-19 13:24:08 +0300

    kwqrina

--- JavaApplication6/src/Calculator.java
+++ JavaApplication6/src/Calculator.java
@@ -8,5 +8,11 @@
  * @author Ярослав
  */
 public class Calculator {
+    public double add (double a, double b){
+        return (a + b);
+    }
+    public double subtract (double a, double b){
+        return (a - b);
+    }
     
 }
--- JavaApplication6/src/CalculatorGUI.java
+++ JavaApplication6/src/CalculatorGUI.java
@@ -7,6 +7,7 @@
  *
  * @author Ярослав
  */
-public class CalculatorGUI {
+public class CalculatorGUI { 
+    
     
 }
